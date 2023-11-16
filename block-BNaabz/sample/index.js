57233875ca2bda9487d05d6c16bbbc4a7783915f
KASHISH <kashu80525@gmail.com>
2023-11-16 16:37:18 +0530

    adding

--- block-BNaabz/sample/index.js
+++ block-BNaabz/sample/index.js
@@ -1,11 +1,12 @@
 var express = require("express");
-var app = express();
+
 var mongoose = require("mongoose");
 
+var app = express();
 app.get("/", (req, res) => {
   res.send("welcome!");
 });
 
-app.listen(3000, () => {
-  console.log("server listening on port 3k");
+app.listen(4000, () => {
+  console.log("server listening on port 4k");
 });
