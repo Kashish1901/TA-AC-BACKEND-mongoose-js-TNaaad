462fbd5f655c9ed9c7a16fdef7652005ad227248
KASHISH <kashu80525@gmail.com>
2023-11-17 16:29:09 +0530

    adding schema using objectId

---
+++ block-BNaacf/sample/models/user.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: { type: string },
+  email: { type: string, lowercase: true },
+  age: { type: number, default: 0 },
+  favourites: [string],
+  marks: [number],
+});
