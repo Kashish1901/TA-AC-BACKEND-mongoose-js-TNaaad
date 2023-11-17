d0ff15ec4728666ded5335aae6f3e746739a8ebe
KASHISH <kashu80525@gmail.com>
2023-11-17 13:14:03 +0530

    adding chnges

--- block-BNaacb/sample/index.js
+++ block-BNaacb/sample/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/test")
+  .connect("mongodb://127.0.0.1:27017/test")
   .then(() => console.log("connected!"))
   .catch((err) => console.log(err));
 var app = express();
