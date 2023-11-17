839e42433a7c654b443fcb8d3c53355b36f2f902
KASHISH <kashu80525@gmail.com>
2023-11-17 12:16:21 +0530

    adding article schema

---
+++ block-BNaacb/sample/index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/test")
+  .then(() => console.log("connected!"))
+  .catch((err) => console.log(err));
+var app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
+app.listen(3100, () => {
+  console.log("server listening on port 3100");
+});
